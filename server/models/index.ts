3b1cf1fa9e5dd611787a59a5117fd614d6158fde
waieitsh <eitsh@naver.com>
2024-03-08 10:33:42 +0900

    index.ts
    - 수정

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,25 +1,11 @@
 'use strict';
 
-import fs from 'fs';
 import path from 'path';
-import { pathToFileURL } from 'node:url';
 import { Sequelize } from 'sequelize';
-import { config } from '../config/config.js';
-const basename = path.basename(path.resolve('dist/models/index.js'));
-const __dirname = path.resolve('dist/models');
 const env = process.env.NODE_ENV || 'development';
+import { config } from '../config/config';
 
-const _config = config(env);
-
-interface IDatabase {
-    [key: string]: any;
-}
-
-interface IModels {
-    [key: string]: any;
-}
-
-const database = {} as IDatabase;
+const _config = config[env];
 
 const sequelize = new Sequelize(
     _config.database,
@@ -38,36 +24,21 @@ const sequelize = new Sequelize(
     },
 );
 
-const resolver = new Promise(function (resolve) {
-    fs.readdirSync(__dirname)
-        .filter(function (file) {
-            return (
-                file.indexOf('.') !== 0 &&
-                file !== basename &&
-                file.slice(-3) === '.js'
-            );
-        })
-        .forEach(function (file) {
-            const url = pathToFileURL(path.join(__dirname, file)).toString();
-            const modelName = file.split('.')[0];
-            import(url).then(function (models) {
-                const model = models[modelName](database.sequelize);
-                database[model.name] = model;
-            });
-            resolve(database);
-        });
+interface Database {
+    [key: string]: { associate: (models: Database) => void };
+}
+
+const database = {} as Database;
+
+const siteUser = require(path.join(__dirname, 'siteUser.js'));
+database[siteUser.name] = siteUser;
+const question = require(path.join(__dirname, 'question.js'));
+database[question.name] = question;
+const answer = require(path.join(__dirname, 'answer.js'));
+database[answer.name] = answer;
+
+Object.keys(database).forEach(function (modelName) {
+    database[modelName].associate(database);
 });
 
-resolver.then(function () {
-    Object.keys(database).forEach(function (modelName) {
-        if (database[modelName].associate) {
-            database[modelName].associate(database);
-        }
-    });
-});
-
-database.sequelize = sequelize;
-database.Sequelize = Sequelize;
-
-export { database, sequelize };
-export type { IDatabase, IModels };
+export { sequelize, database };
