ac5210b33a2334182c45efbeb6eed39d461654d8
waieitsh <eitsh@naver.com>
2024-03-10 21:56:28 +0900

    index.ts
    - cors & express.json 적용

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,27 @@
 'use strict';
 
-import express, { Express } from 'express';
+import express from 'express';
 import router from './router/signup.js';
 import { sequelize } from './models/index.js';
+import cors from 'cors';
 
-const app: Express = express();
+const app = express();
 const port: number = 5000;
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.json());
 
 sequelize
     .authenticate()
     .then(function () {
         console.log('Connection has been established successfully.');
     })
-    .catch(function (error: Error) {
+    .catch(function (error) {
         console.error('Unable to connect to the database:', error);
     });
 
 app.use('/user', router);
 
 app.listen(port, function () {
-    console.log(`the server is running on http://localhost:${port}`);
+    console.log(`The server is running on http://localhost:${port}`);
 });
