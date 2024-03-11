ab19c2bfd4a9b29ff561ea5cb967e8517514a2e8
waieitsh <eitsh@naver.com>
2024-03-11 20:32:44 +0900

    signup.ts
    - bcrypt 적용

--- server/controller/signup.ts
+++ server/controller/signup.ts
@@ -1,9 +1,22 @@
 'use strict';
 
 import { Request, Response } from 'express';
+import { SiteUser } from '../models/siteUser.js';
+import bcrypt from 'bcrypt';
 
-function signup(req: Request, res: Response) {
-    return res.json({ status: true });
+function signup(request: Request, response: Response) {
+    const { username, password1, email } = request.body;
+    const saltRounds = 10;
+
+    bcrypt.hash(password1, saltRounds, async function (error, hash) {
+        await SiteUser.create({
+            username: username,
+            password: hash,
+            email: email,
+        });
+    });
+
+    return response.json({ message: 'signup succeeded' });
 }
 
 export default signup;
