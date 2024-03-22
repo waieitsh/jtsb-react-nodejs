4a4282982b96547562889afe5e62a21a1d55e52d
waieitsh <eitsh@naver.com>
2024-03-22 13:57:19 +0900

    QuestionList.tsx
    - 파일 수정

--- client/src/components/QuestionList.tsx
+++ client/src/components/QuestionList.tsx
@@ -1,27 +1,30 @@
 import { useEffect, useState } from 'react';
-import { IAnswerListDetails, IFirst, IQuestion } from '../types';
+import { IQuestion } from '../types';
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 
 function QuestionList() {
     const [searchParams] = useSearchParams('');
-    const params = searchParams.get('page');
-    const [question, setQuestion] = useState<IQuestion>({} as IQuestion);
+    const param = Number(searchParams.get('page'));
+    const [questions, setQuestions] = useState<IQuestion>({} as IQuestion);
+    const [page, setPage] = useState<number>(param);
+    const [totalPages, setTotalPages] = useState<number>(0);
 
     useEffect(
         function () {
             axios
-                .get(`https://localhost:8080/question/list`, {
-                    params: { page: params },
+                .get(`http://localhost:5000/question/list`, {
+                    params: { page: param },
                 })
                 .then(function (response) {
-                    setQuestion(response.data);
+                    setQuestions(response.data);
+                    setTotalPages(response.data.count / 10);
                 })
                 .catch(function (error) {
                     console.log(`error >> ${error}`);
                 });
         },
-        [params],
+        [param],
     );
 
     return (
@@ -37,100 +40,59 @@ function QuestionList() {
                 </thead>
 
                 <tbody>
-                    {question.first !== undefined
-                        ? question.first.map((data: IFirst, index: number) => (
-                              <tr className="text-center" key={index}>
-                                  <td>{data.id}</td>
-                                  <td className="text-start">
-                                      <a href={`/question/details/${data.id}`}>
-                                          {data.subject}
-                                      </a>
-                                      <span className="text-danger small ms-2">
-                                          {question.third.map(
-                                              (data2: IAnswerListDetails) =>
-                                                  data.id === data2.id &&
-                                                  data2.size > 0
-                                                      ? data2.size
-                                                      : undefined,
-                                          )}
-                                      </span>
-                                  </td>
-                                  <td></td>
-                                  <td>{data.createDate}</td>
-                              </tr>
-                          ))
-                        : null}
+                    {questions.data?.map((question) => (
+                        <tr className="text-center" key={question.id}>
+                            <td>{question.id}</td>
+                            <td className="text-start">
+                                <a href={`/question/details/${question.id}`}>{question.subject}</a>
+                                <span className="text-danger small ms-2">
+                                    {question.answers.map((answer) =>
+                                        question.id === answer.question_id ? question.answers.length : undefined,
+                                    )}
+                                </span>
+                            </td>
+                            <td>{question.site_user.username}</td>
+                            <td>{question.createdAt}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
             <div>
-                {question.second !== undefined ? (
-                    <ul className="pagination justify-content-center">
-                        <li
-                            className={
-                                question.second[0].hasPrevious
-                                    ? 'page-item'
-                                    : 'page-item disabled'
-                            }
-                        >
-                            <a
-                                className="page-link"
-                                href={`?page=${question.second[0].number - 1}`}
-                            >
-                                <span>이전</span>
-                            </a>
-                        </li>
-                        {Array.from({
-                            length: question.second[0].totalPages,
-                        }).map((_, index: number) => (
-                            <li
-                                className={
-                                    index === question.second[0].number
-                                        ? 'page-item active'
-                                        : 'page-item'
-                                }
-                                key={index}
-                            >
-                                <a
-                                    className="page-link"
-                                    href={`?page=${index}`}
-                                >
-                                    {index + 1}
-                                </a>
-                            </li>
-                        ))}
-                        {Array.from({
-                            length: question.second[0].totalPages,
-                        }).map((_, index: number) => (
-                            <li
-                                className={
-                                    index >= question.second[0].number - 5 &&
-                                    index <= question.second[0].number + 5
-                                        ? 'page-item active'
-                                        : 'page-item'
-                                }
-                                key={index}
-                            />
-                        ))}
-                        <li
-                            className={
-                                question.second[0].hasNext
-                                    ? 'page-item '
-                                    : 'page-item disabled'
-                            }
-                        >
-                            <a
-                                className="page-link"
-                                href={`?page=${question.second[0].number + 1}`}
-                            >
-                                <span>다음</span>
-                            </a>
-                        </li>
-                    </ul>
-                ) : null}
-                <a href={'/question/create'} className="btn btn-primary">
-                    질문 등록하기
+                <ul className="pagination justify-content-center">
+                    <li className={page > 0 ? 'page-item' : 'page-item disabled'}>
+                        <a className="page-link" href={`?page=${page - 1}`}>
+                            <span>이전</span>
+                        </a>
+                    </li>
+                    <li style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '30%' }}>
+                        <input
+                            type="text"
+                            name="page"
+                            className="page-link"
+                            defaultValue={page + 1}
+                            style={{ width: '20%', textAlign: 'center' }}
+                            onChange={(e) => setPage(Number(e.target.value))}
+                        />
+                        <div style={{ width: '10%', paddingLeft: '3.5%' }}>/</div>
+                        <div className="page-link" style={{ width: '20%', textAlign: 'center' }}>
+                            {totalPages}
+                        </div>
+                    </li>
+                    <li className={(page + 1) * 10 < questions.count ? 'page-item ' : 'page-item disabled'}>
+                        <a className="page-link" href={`?page=${page + 1}`}>
+                            <span>다음</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '-1%' }}>
+                <a className="btn btn-primary" href={`?page=${page - 1}`}>
+                    <span>이동</span>
                 </a>
             </div>
+            <a className="btn btn-primary" href={'/question/create'}>
+                질문 등록하기
+            </a>
         </div>
     );
 }
