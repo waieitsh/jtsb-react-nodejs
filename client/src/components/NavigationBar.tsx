d2c1366b404b84777ff28ae5ccb48376723f051d
waieitsh <eitsh@naver.com>
2024-03-27 00:55:40 +0900

    NavigationBar.tsx
    - 네비게이션 컴포넌트 로직 수정

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Buffer } from 'buffer';
+import { useNavigate } from 'react-router-dom';
+
 function NavigationBar() {
+    const [loginUser, setLoginUser] = useState<string>('');
+    const [sessionUser, setSessionUser] = useState<string>('');
+    const navigate = useNavigate();
+
+    useEffect(
+        function () {
+            const _loginUser = Buffer.from((localStorage.getItem('loginUser') as string) || '', 'base64').toString(
+                'ascii',
+            );
+            const _sessionUser = Buffer.from((localStorage.getItem('sessionUser') as string) || '', 'base64').toString(
+                'ascii',
+            );
+
+            setLoginUser(_loginUser);
+            setSessionUser(_sessionUser);
+        },
+        [loginUser, sessionUser, navigate],
+    );
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
             <div className="container-fluid">
@@ -19,9 +42,15 @@ function NavigationBar() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" href="/user/login">
-                                로그인
-                            </a>
+                            {loginUser !== sessionUser || (loginUser === '' && sessionUser === '') ? (
+                                <a className="nav-link" href="/user/login">
+                                    로그인
+                                </a>
+                            ) : (
+                                <a className="nav-link" href="/user/logout">
+                                    로그아웃
+                                </a>
+                            )}
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/user/signup">
