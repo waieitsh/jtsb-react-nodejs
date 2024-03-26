5142def6dbc2edf24b7b62ce971c132ea4eb914e
waieitsh <eitsh@naver.com>
2024-03-27 01:05:25 +0900

    verifyToken.ts
    - 로직 수정

--- server/middleware/verifyToken.ts
+++ server/middleware/verifyToken.ts
@@ -1,20 +1,27 @@
 import { readFileSync } from 'node:fs';
-import jwt from 'jsonwebtoken';
+import jwt, { Jwt, JwtPayload, VerifyErrors } from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import path from 'node:path';
 
-const publicKey = readFileSync(path.resolve() + '/public.pem');
-
 function verifyToken(request: Request, response: Response, next: NextFunction) {
+    const publicKey = readFileSync(path.resolve() + '/public.pem');
     const { token } = request.body;
 
-    // jwt.verify(token, publicKey, function (error: VerifyErrors | null, decoded: Jwt | JwtPayload | string | undefined) {
-    //     console.log(decoded);
-    // });
+    if (token) {
+        jwt.verify(
+            token,
+            publicKey,
+            function (error: VerifyErrors | null, decoded: Jwt | JwtPayload | string | undefined) {
+                if (error?.message === 'jwt expired') {
+                    return response.status(400).json({ message: error?.message });
+                }
 
-    jwt.verify(token, publicKey, function (error: any, decoded: any) {
-        console.log(decoded);
-    });
+                response.locals.username = (decoded as JwtPayload).aud
+            },
+        );
+
+        next();
+    }
 }
 
 export default verifyToken;
