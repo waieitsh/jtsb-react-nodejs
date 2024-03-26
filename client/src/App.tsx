25766acc63b5945a2fa6dc284676969bf818b5b0
waieitsh <eitsh@naver.com>
2024-03-27 00:51:34 +0900

    App.tsx
    - lazy import 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { Suspense, lazy, useEffect, useState } from 'react';
+import axios from 'axios';
+import { BindingResult } from './types';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import QuestionList from './components/QuestionList';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import QuestionDetails from './components/QuestionDetails';
-import QuestionForm from './components/QuestionForm';
-import SignupForm from './components/SignupForm';
-import LoginForm from './components/LoginForm';
-import PageNotFound from './components/PageNotFound';
+import { Buffer } from 'buffer';
+
+const QuestionDetail = lazy(() => import('./components/QuestionDetail'));
+const QuestionForm = lazy(() => import('./components/QuestionForm'));
+const SignupForm = lazy(() => import('./components/SignupForm'));
+const LoginForm = lazy(() => import('./components/LoginForm'));
+const PageNotFound = lazy(() => import('./components/PageNotFound'));
+const Logout = lazy(() => import('./components/Logout'));
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<QuestionList />} />
-                <Route path="/question/list" element={<QuestionList />} />
-                <Route
-                    path="/question/details/:id"
-                    element={<QuestionDetails />}
-                />
-                <Route path="/question/create" element={<QuestionForm />} />
-                <Route path="/user/signup" element={<SignupForm />} />
-                <Route path="/user/login" element={<LoginForm />} />
-                <Route path="/pageNotFound" element={<PageNotFound />} />
-            </Routes>
+            <Suspense fallback={<div>loading...</div>}>
+                <NavigationBar />
+                <Routes>
+                    <Route path="/" element={<QuestionList />} />
+                    <Route path="/question/list" element={<QuestionList />} />
+                    <Route path="/question/details/:id" element={<QuestionDetail />} />
+                    <Route path="/question/create" element={<QuestionForm />} />
+                    <Route path="/user/signup" element={<SignupForm />} />
+                    <Route path="/user/login" element={<LoginForm />} />
+                    <Route path="/user/logout" element={<Logout />} />
+                    <Route path="/pageNotFound" element={<PageNotFound />} />
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     );
 }
